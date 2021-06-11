12a952ab492310404476caf6298de95f6c5a8fe6
jeff <jeff@radar.io>
2021-06-11 16:32:31 -0400

    pip3!!!

--- ci/install_dependencies.sh
+++ ci/install_dependencies.sh
@@ -4,7 +4,8 @@ set -ex
 
 # CircleCI comes with pyenv
 pyenv install --skip-existing 3.5.2
-pyenv local 3.5.2
+pyenv global 3.5.2
 
 # beanstalk CLI
+pip3 install -U pip3
 pip3 install awsebcli==3.20.0
